ec60b112b879c495ff9c4d7c64ba16a00d8d81a9
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-06 17:30:03 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,31 +1,29 @@
 local OPA = false
 
-Citizen.CreateThread( function()
+CreateThread(function()
     while true do 
-        Citizen.Wait( 1 )
+        Wait(1)
+        local ped = PlayerPedId()
+        if (DoesEntityExist(ped) and not IsEntityDead(ped)) then 
+            DisableControlAction(0, 36, true)
 
-        local ped = GetPlayerPed( -1 )
+            if (not IsPauseMenuActive()) then 
+                if (IsDisabledControlJustPressed(0, 36)) then 
+                    RequestAnimSet("move_ped_crouched")
 
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
-            DisableControlAction( 0, 36, true )
-
-            if ( not IsPauseMenuActive() ) then 
-                if ( IsDisabledControlJustPressed( 0, 36 ) ) then 
-                    RequestAnimSet( "move_ped_crouched" )
-
-                    while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do 
-                        Citizen.Wait( 100 )
+                    while (not HasAnimSetLoaded("move_ped_crouched")) do 
+                        Citizen.Wait(100)
                     end 
 
-                    if ( OPA == true ) then 
-                        ResetPedMovementClipset( ped, 0 )
+                    if (OPA) then 
+                        ResetPedMovementClipset(ped, 0)
                         OPA = false 
-                    elseif ( OPA == false ) then
-                        SetPedMovementClipset( ped, "move_ped_crouched", 0.25 )
+                    else
+                        SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
                         OPA = true 
                     end 
                 end
             end 
         end 
     end
-end )
+end)
